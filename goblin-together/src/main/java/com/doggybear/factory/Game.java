fe8831faf14679ff6aee3b0bbbfe8c8ac3923463
kevin083177 <kevin083177@gmail.com>
2025-05-14 11:21:30 +0800

    chore: update for lava component

--- goblin-together/src/main/java/com/doggybear/factory/Game.java
+++ goblin-together/src/main/java/com/doggybear/factory/Game.java
@@ -12,11 +12,16 @@ import com.almasb.fxgl.physics.box2d.dynamics.BodyType;
 import com.almasb.fxgl.physics.box2d.dynamics.FixtureDef;
 import com.almasb.fxgl.texture.Texture;
 import com.doggybear.component.Goblin;
+import com.doggybear.component.Lava;
 import com.doggybear.component.Platform;
+import com.doggybear.type.EntityType;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.scene.Group;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
 
 import static com.almasb.fxgl.dsl.FXGL.entityBuilder;
 
@@ -57,34 +62,36 @@ public class Game implements EntityFactory {
     @Spawns("platform")
     public Entity newPlatform(SpawnData data) {
         PhysicsComponent physics = new PhysicsComponent();
-        
         FixtureDef fd = new FixtureDef();
         fd.setFriction(1.0f);
         fd.setDensity(1.0f);
         physics.setFixtureDef(fd);
-        
-        physics.setBodyType(BodyType.STATIC);
-        
-        int width = data.get("width");
-        int height = data.get("height");
-        
-        Platform platform = new Platform(width, height);
-        
+
+        int width = ((Number) data.get("width")).intValue();
+        int height = ((Number) data.get("height")).intValue();
+
+        Platform platform = new Platform(width, height)
+            .setPhysics(physics);
+
         if (data.hasKey("color")) {
-            platform.setColor(data.get("color"));
+            platform.setColor((Color) data.get("color"));
         }
-        
+
         if (data.hasKey("moving")) {
-            double speed = data.hasKey("moveSpeed") ? data.get("moveSpeed") : 100.0;
-            double distance = data.hasKey("moveDistance") ? data.get("moveDistance") : 200.0;
+            physics.setBodyType(BodyType.KINEMATIC);
+
+            double speed = ((Number) data.get("speed")).doubleValue();
+            double distance = ((Number) data.get("distance")).doubleValue();
             platform.setMoving(speed, distance);
+        } else {
+            physics.setBodyType(BodyType.STATIC);
         }
-        
+
         return entityBuilder(data)
                 .type(EntityType.PLATFORM)
                 .bbox(new HitBox(BoundingShape.box(width, height)))
                 .with(physics)
-                .viewWithBBox(new Rectangle(width, height, Color.BROWN))
+                .view(platform.getViewNode())
                 .with(platform)
                 .with(new CollidableComponent(true))
                 .build();
@@ -92,52 +99,67 @@ public class Game implements EntityFactory {
 
     @Spawns("lava")
     public Entity newLava(SpawnData data) {
-        int width, height;
-        
-        Object widthObj = data.get("width");
-        Object heightObj = data.get("height");
-        
-        if (widthObj instanceof Number) {
-            width = ((Number)widthObj).intValue();
-        } else {
-            width = Integer.parseInt(widthObj.toString());
-        }
-        
-        if (heightObj instanceof Number) {
-            height = ((Number)heightObj).intValue();
-        } else {
-            height = Integer.parseInt(heightObj.toString());
-        }
-        
+        int width = data.get("width");
+        int height = data.get("height");
+
         Group lavaGroup = new Group();
-        
-        int blockSize = 40; // 40x40
-        
+
+        int blockSize = 40;
         int columns = (width + blockSize - 1) / blockSize;
         int rows = (height + blockSize - 1) / blockSize;
-        
+
         for (int row = 0; row < rows; row++) {
             for (int col = 0; col < columns; col++) {
-                Texture lavaBlock = FXGL.getAssetLoader().loadTexture("lava.png");
-                
-                lavaBlock.setFitWidth(blockSize);
-                lavaBlock.setFitHeight(blockSize);
-                
-                lavaBlock.setTranslateX(col * blockSize);
-                lavaBlock.setTranslateY(row * blockSize);
-                
-                lavaGroup.getChildren().add(lavaBlock);
+                Group animatedBlock = createLavaBlock(blockSize);
+
+                animatedBlock.setTranslateX(col * blockSize);
+                animatedBlock.setTranslateY(row * blockSize);
+
+                lavaGroup.getChildren().add(animatedBlock);
             }
         }
-        
-        Rectangle clipRect = new Rectangle(width, height);
-        lavaGroup.setClip(clipRect);
-        
+
+        lavaGroup.setClip(new Rectangle(width, height));
+
         return entityBuilder(data)
                 .type(EntityType.LAVA)
                 .bbox(new HitBox(BoundingShape.box(width, height)))
                 .view(lavaGroup)
                 .with(new CollidableComponent(true))
+                .with(new Lava(lavaGroup))
                 .build();
+    }
+
+    private Group createLavaBlock(int blockSize) {
+        Group group = new Group();
+
+        int frameCount = 2; // lava_1.png ~ lava_3.png
+        Texture[] frames = new Texture[frameCount];
+
+        for (int i = 0; i < frameCount; i++) {
+            frames[i] = FXGL.getAssetLoader().loadTexture("lava_" + (i + 1) + ".png");
+            frames[i].setFitWidth(blockSize);
+            frames[i].setFitHeight(blockSize);
+            frames[i].setVisible(i == 0);
+            group.getChildren().add(frames[i]);
+        }
+
+        Timeline timeline = new Timeline();
+        timeline.setCycleCount(Timeline.INDEFINITE);
+
+        for (int i = 0; i < frameCount; i++) {
+            int current = i;
+            KeyFrame frame = new KeyFrame(Duration.seconds(0.25 * i), e -> {
+                for (int j = 0; j < frameCount; j++) {
+                    frames[j].setVisible(j == current);
+                }
+            });
+            timeline.getKeyFrames().add(frame);
+        }
+
+        timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(0.25 * frameCount))); // 結束回到起點
+        timeline.play();
+
+        return group;
     }
 }
