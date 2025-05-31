66edc1c7e4bbc9f1f22a695cdf4e4354b26117a8
kevin083177 <kevin083177@gmail.com>
2025-05-31 17:51:23 +0800

    refactor: spilt MovingPlatform

--- goblin-together/src/main/java/com/doggybear/component/Platform.java
+++ goblin-together/src/main/java/com/doggybear/component/Platform.java
@@ -1,22 +1,14 @@
 package com.doggybear.component;
 
 import com.almasb.fxgl.entity.component.Component;
-import com.almasb.fxgl.physics.PhysicsComponent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class Platform extends Component {
-    private PhysicsComponent physics;
     private Rectangle viewNode;
-
     private Color color = Color.RED;
     private int width;
     private int height;
-
-    private boolean isMoving = false;
-    private double moveSpeed;
-    private double moveDistance;
-    private double initialX;
 
     public Platform(int width, int height) {
         this.width = width;
@@ -24,30 +16,8 @@ public class Platform extends Component {
         this.viewNode = new Rectangle(width, height, color);
     }
 
-    public Platform setPhysics(PhysicsComponent physics) {
-        this.physics = physics;
-        return this;
-    }
-
     public Rectangle getViewNode() {
         return viewNode;
-    }
-
-    @Override
-    public void onAdded() {
-        initialX = entity.getX();
-    }
-
-    @Override
-    public void onUpdate(double tpf) {
-        if (!isMoving || physics == null) return;
-
-        physics.setLinearVelocity(moveSpeed, 0);
-
-        if (Math.abs(entity.getX() - initialX) > moveDistance) {
-            moveSpeed = -moveSpeed;
-            physics.setLinearVelocity(moveSpeed, 0);
-        }
     }
 
     public Platform setColor(Color color) {
@@ -58,17 +28,7 @@ public class Platform extends Component {
         return this;
     }
 
-    public void setMoving(double speed, double distance) {
-        this.moveSpeed = speed;
-        this.moveDistance = distance;
-        this.isMoving = true;
-    }
-
     public Color getColor() {
         return color;
-    }
-
-    public boolean isMoving() {
-        return isMoving;
     }
 }
