1307f19e9748589fab0c834e2131e8d82976b73b
kevin083177 <kevin083177@gmail.com>
2025-05-31 18:37:03 +0800

    refactor: unify collision handling for entity types

--- goblin-together/src/main/java/com/doggybear/controller/PhysicsController.java
+++ goblin-together/src/main/java/com/doggybear/controller/PhysicsController.java
@@ -23,265 +23,153 @@ public class PhysicsController {
     public void initPhysics() {
         PhysicsWorld physicsWorld = getPhysicsWorld();
         
-        // 玩家碰撞平台
-        physicsWorld.addCollisionHandler(new CollisionHandler(EntityType.GOBLIN, EntityType.PLATFORM) {
-            @Override
-            protected void onCollisionBegin(Entity goblin, Entity platform) {
-                if (goblin.getBottomY() <= platform.getY() + 5) {
-                    goblin.getComponent(Goblin.class).onGroundCollision();
+        // 平台碰撞處理
+        for (EntityType playerType : EntityType.playerTypes) {
+            physicsWorld.addCollisionHandler(new CollisionHandler(playerType, EntityType.PLATFORM) {
+                @Override
+                protected void onCollisionBegin(Entity player, Entity platform) {
+                    if (player.getBottomY() <= platform.getY() + 5) {
+                        player.getComponent(Goblin.class).onGroundCollision();
+                    }
                 }
-            }
-        });
-       
-        physicsWorld.addCollisionHandler(new CollisionHandler(EntityType.GOBLIN2, EntityType.PLATFORM) {
-            @Override
-            protected void onCollisionBegin(Entity goblin2, Entity platform) {
-                if (goblin2.getBottomY() <= platform.getY() + 5) {
-                    goblin2.getComponent(Goblin.class).onGroundCollision();
+            });
+        }
+
+        // 彈跳平台碰撞處理
+        for (EntityType playerType : EntityType.playerTypes) {
+            physicsWorld.addCollisionHandler(new CollisionHandler(playerType, EntityType.BOUNCE) {
+                @Override
+                protected void onCollisionBegin(Entity player, Entity bounce) {
+                    bounce.getComponent(BouncePlatform.class).bounce(player);
                 }
-            }
-        });
-
-        physicsWorld.addCollisionHandler(new CollisionHandler(EntityType.GOBLIN, EntityType.BOUNCE) {
-            @Override
-            protected void onCollisionBegin(Entity player, Entity bounce) {
-                bounce.getComponent(BouncePlatform.class).bounce(player);
-            }
-        });
-
-        physicsWorld.addCollisionHandler(new CollisionHandler(EntityType.GOBLIN2, EntityType.BOUNCE) {
-            @Override
-            protected void onCollisionBegin(Entity player, Entity bounce) {
-                bounce.getComponent(BouncePlatform.class).bounce(player);
-            }
-        });
+            });
+        }
 
         // 移動平台碰撞處理
-        physicsWorld.addCollisionHandler(new CollisionHandler(EntityType.GOBLIN, EntityType.MOVING) {
-            @Override
-            protected void onCollisionBegin(Entity goblin, Entity platform) {
-                platform.getComponent(MovingPlatform.class).setPlayerOnPlatform(true);
-                goblin.getComponent(Goblin.class).onGroundCollision();
-            }
-            @Override
-            protected void onCollisionEnd(Entity goblin, Entity platform) {
-                platform.getComponent(MovingPlatform.class).setPlayerOnPlatform(false);
-            }
-        });
-
-        physicsWorld.addCollisionHandler(new CollisionHandler(EntityType.GOBLIN2, EntityType.MOVING) {
-            @Override
-            protected void onCollisionBegin(Entity goblin, Entity platform) {
-                platform.getComponent(MovingPlatform.class).setPlayerOnPlatform(true);
-                goblin.getComponent(Goblin.class).onGroundCollision();
-            }
-            @Override
-            protected void onCollisionEnd(Entity goblin, Entity platform) {
-                platform.getComponent(MovingPlatform.class).setPlayerOnPlatform(false);
-            }
-        });
+        for (EntityType playerType : EntityType.playerTypes) {
+            physicsWorld.addCollisionHandler(new CollisionHandler(playerType, EntityType.MOVING) {
+                @Override
+                protected void onCollisionBegin(Entity player, Entity platform) {
+                    platform.getComponent(MovingPlatform.class).setPlayerOnPlatform(true);
+                    player.getComponent(Goblin.class).onGroundCollision();
+                }
+                
+                @Override
+                protected void onCollisionEnd(Entity player, Entity platform) {
+                    platform.getComponent(MovingPlatform.class).setPlayerOnPlatform(false);
+                }
+            });
+        }
 
         // 消失平台碰撞處理
-        physicsWorld.addCollisionHandler(new CollisionHandler(EntityType.GOBLIN, EntityType.DISAPPEARING) {
-            @Override
-            protected void onCollisionBegin(Entity goblin, Entity platform) {
-                DisappearingPlatform disappearing = platform.getComponent(DisappearingPlatform.class);
-                if (disappearing != null && disappearing.isVisible()) {
-                    goblin.getComponent(Goblin.class).onGroundCollision();
-                    
-                    if (goblin.getBottomY() <= platform.getY() + 5) {
-                        disappearing.setPlayerOnPlatform(true);
+        for (EntityType playerType : EntityType.playerTypes) {
+            physicsWorld.addCollisionHandler(new CollisionHandler(playerType, EntityType.DISAPPEARING) {
+                @Override
+                protected void onCollisionBegin(Entity player, Entity platform) {
+                    DisappearingPlatform disappearing = platform.getComponent(DisappearingPlatform.class);
+                    if (disappearing != null && disappearing.isVisible()) {
+                        player.getComponent(Goblin.class).onGroundCollision();
+                        
+                        if (player.getBottomY() <= platform.getY() + 5) {
+                            disappearing.setPlayerOnPlatform(true);
+                        }
                     }
                 }
-            }
-            
-            @Override
-            protected void onCollision(Entity goblin, Entity platform) {
-                DisappearingPlatform disappearing = platform.getComponent(DisappearingPlatform.class);
-                if (disappearing != null && !disappearing.isVisible()) {
-                    goblin.getComponent(Goblin.class).setOnGround(false);
-                    disappearing.setPlayerOnPlatform(false);
-                }
-            }
-            
-            @Override
-            protected void onCollisionEnd(Entity goblin, Entity platform) {
-                DisappearingPlatform disappearing = platform.getComponent(DisappearingPlatform.class);
-                if (disappearing != null) {
-                    disappearing.setPlayerOnPlatform(false);
-                }
-            }
-        });
-
-        physicsWorld.addCollisionHandler(new CollisionHandler(EntityType.GOBLIN2, EntityType.DISAPPEARING) {
-            @Override
-            protected void onCollisionBegin(Entity goblin2, Entity platform) {
-                DisappearingPlatform disappearing = platform.getComponent(DisappearingPlatform.class);
-                if (disappearing != null && disappearing.isVisible()) {
-                    goblin2.getComponent(Goblin.class).onGroundCollision();
-                    
-                    if (goblin2.getBottomY() <= platform.getY() + 5) {
-                        disappearing.setPlayerOnPlatform(true);
+                
+                @Override
+                protected void onCollision(Entity player, Entity platform) {
+                    DisappearingPlatform disappearing = platform.getComponent(DisappearingPlatform.class);
+                    if (disappearing != null && !disappearing.isVisible()) {
+                        player.getComponent(Goblin.class).setOnGround(false);
+                        disappearing.setPlayerOnPlatform(false);
                     }
                 }
-            }
-            
-            @Override
-            protected void onCollision(Entity goblin2, Entity platform) {
-                DisappearingPlatform disappearing = platform.getComponent(DisappearingPlatform.class);
-                if (disappearing != null && !disappearing.isVisible()) {
-                    goblin2.getComponent(Goblin.class).setOnGround(false);
-                    disappearing.setPlayerOnPlatform(false);
-                }
-            }
-            
-            @Override
-            protected void onCollisionEnd(Entity goblin2, Entity platform) {
-                DisappearingPlatform disappearing = platform.getComponent(DisappearingPlatform.class);
-                if (disappearing != null) {
-                    disappearing.setPlayerOnPlatform(false);
-                }
-            }
-        });
-
-        // 冰面碰撞處理
-        physicsWorld.addCollisionHandler(new CollisionHandler(EntityType.GOBLIN, EntityType.ICE) {
-            @Override
-            protected void onCollisionBegin(Entity goblin, Entity ice) {
-                if (isStandingOnIce(goblin, ice)) {
-                    goblin.getComponent(Goblin.class).setOnIce(true);
-                    goblin.getComponent(Goblin.class).onGroundCollision();
-                }
-            }
-            
-            @Override
-            protected void onCollisionEnd(Entity goblin, Entity ice) {
-                goblin.getComponent(Goblin.class).setOnIce(false);
-            }
-            
-            private boolean isStandingOnIce(Entity player, Entity ice) {
-                double playerBottom = player.getBottomY();
-                double iceTop = ice.getY();
                 
-                return playerBottom >= iceTop - 5 && 
-                    playerBottom <= iceTop + 5 &&
-                    player.getX() < ice.getRightX() &&
-                    player.getRightX() > ice.getX();
-            }
-        });
-
-        physicsWorld.addCollisionHandler(new CollisionHandler(EntityType.GOBLIN2, EntityType.ICE) {
-            @Override
-            protected void onCollisionBegin(Entity goblin2, Entity ice) {
-                if (isStandingOnIce(goblin2, ice)) {
-                    goblin2.getComponent(Goblin.class).setOnIce(true);
-                    goblin2.getComponent(Goblin.class).onGroundCollision();
+                @Override
+                protected void onCollisionEnd(Entity player, Entity platform) {
+                    DisappearingPlatform disappearing = platform.getComponent(DisappearingPlatform.class);
+                    if (disappearing != null) {
+                        disappearing.setPlayerOnPlatform(false);
+                    }
                 }
-            }
-            
-            @Override
-            protected void onCollisionEnd(Entity goblin2, Entity ice) {
-                goblin2.getComponent(Goblin.class).setOnIce(false);
-            }
-            
-            private boolean isStandingOnIce(Entity player, Entity ice) {
-                double playerBottom = player.getBottomY();
-                double iceTop = ice.getY();
+            });
+        }
+
+        // 冰平台碰撞處理
+        for (EntityType playerType : EntityType.playerTypes) {
+            physicsWorld.addCollisionHandler(new CollisionHandler(playerType, EntityType.ICE) {
+                @Override
+                protected void onCollisionBegin(Entity player, Entity ice) {
+                    if (isStandingOnIce(player, ice)) {
+                        player.getComponent(Goblin.class).setOnIce(true);
+                        player.getComponent(Goblin.class).onGroundCollision();
+                    }
+                }
                 
-                return playerBottom >= iceTop - 5 && 
-                    playerBottom <= iceTop + 5 &&
-                    player.getX() < ice.getRightX() &&
-                    player.getRightX() > ice.getX();
-            }
-        });
+                @Override
+                protected void onCollisionEnd(Entity player, Entity ice) {
+                    player.getComponent(Goblin.class).setOnIce(false);
+                }
+                
+                private boolean isStandingOnIce(Entity player, Entity ice) {
+                    double playerBottom = player.getBottomY();
+                    double iceTop = ice.getY();
+                    
+                    return playerBottom >= iceTop - 5 && 
+                        playerBottom <= iceTop + 5 &&
+                        player.getX() < ice.getRightX() &&
+                        player.getRightX() > ice.getX();
+                }
+            });
+        }
+
+        // 火焰平台碰撞處理
+        for (EntityType playerType : EntityType.playerTypes) {
+            physicsWorld.addCollisionHandler(new CollisionHandler(playerType, EntityType.FIRE) {
+                @Override
+                protected void onCollisionBegin(Entity player, Entity firePlatform) {
+                    FirePlatform platform = firePlatform.getComponent(FirePlatform.class);
+                    if (platform != null && platform.isFireState()) {
+                        if (gameOverCallback != null) {
+                            gameOverCallback.onGameOver();
+                        }
+                    }
+                }
+            });
+        }
 
         // 岩漿碰撞處理
-        physicsWorld.addCollisionHandler(new CollisionHandler(EntityType.GOBLIN, EntityType.LAVA) {
-            @Override
-            protected void onCollisionBegin(Entity goblin, Entity lava) {
-                if (gameOverCallback != null) {
-                    gameOverCallback.onGameOver();
+        for (EntityType playerType : EntityType.playerTypes) {
+            physicsWorld.addCollisionHandler(new CollisionHandler(playerType, EntityType.LAVA) {
+                @Override
+                protected void onCollisionBegin(Entity player, Entity lava) {
+                    if (gameOverCallback != null) {
+                        gameOverCallback.onGameOver();
+                    }
                 }
-            }
-        });
+            });
+        }
 
-        physicsWorld.addCollisionHandler(new CollisionHandler(EntityType.GOBLIN2, EntityType.LAVA) {
-            @Override
-            protected void onCollisionBegin(Entity goblin2, Entity lava) {
-                if (gameOverCallback != null) {
-                    gameOverCallback.onGameOver();
+        // 尖刺碰撞處理
+        for (EntityType playerType : EntityType.playerTypes) {
+            physicsWorld.addCollisionHandler(new CollisionHandler(playerType, EntityType.SPIKE) {
+                @Override
+                protected void onCollisionBegin(Entity player, Entity spike) {
+                    if (gameOverCallback != null) {
+                        gameOverCallback.onGameOver();
+                    }
                 }
-            }
-        });
+            });
+        }
         
-        // 刺的碰撞處理
-        physicsWorld.addCollisionHandler(new CollisionHandler(EntityType.GOBLIN, EntityType.SPIKE) {
-            @Override
-            protected void onCollisionBegin(Entity goblin, Entity spike) {
-                if (gameOverCallback != null) {
-                    gameOverCallback.onGameOver();
+        // 子彈與平台的碰撞
+        for (EntityType platformType : EntityType.removeBulletTypes) {
+            physicsWorld.addCollisionHandler(new CollisionHandler(EntityType.BULLET, platformType) {
+                @Override
+                protected void onCollisionBegin(Entity bullet, Entity platform) {
+                    bullet.removeFromWorld();
                 }
-            }
-        });
-        
-        physicsWorld.addCollisionHandler(new CollisionHandler(EntityType.GOBLIN2, EntityType.SPIKE) {
-            @Override
-            protected void onCollisionBegin(Entity goblin2, Entity spike) {
-                if (gameOverCallback != null) {
-                    gameOverCallback.onGameOver();
-                }
-            }
-        });
-
-        // 弓箭的碰撞處理
-        physicsWorld.addCollisionHandler(new CollisionHandler(EntityType.GOBLIN, EntityType.BULLET) {
-            @Override
-            protected void onCollisionBegin(Entity goblin, Entity arrow) {
-                arrow.removeFromWorld();
-                if (gameOverCallback != null) {
-                    gameOverCallback.onGameOver();
-                }
-            }
-        });
-        
-        physicsWorld.addCollisionHandler(new CollisionHandler(EntityType.GOBLIN2, EntityType.BULLET) {
-            @Override
-            protected void onCollisionBegin(Entity goblin2, Entity arrow) {
-                arrow.removeFromWorld();
-                if (gameOverCallback != null) {
-                    gameOverCallback.onGameOver();
-                }
-            }
-        });
-        
-        // 弓箭與平台的碰撞
-        physicsWorld.addCollisionHandler(new CollisionHandler(EntityType.BULLET, EntityType.PLATFORM) {
-            @Override
-            protected void onCollisionBegin(Entity arrow, Entity platform) {
-                arrow.removeFromWorld();
-            }
-        });
-        
-        physicsWorld.addCollisionHandler(new CollisionHandler(EntityType.BULLET, EntityType.MOVING) {
-            @Override
-            protected void onCollisionBegin(Entity arrow, Entity platform) {
-                arrow.removeFromWorld();
-            }
-        });
-        
-        physicsWorld.addCollisionHandler(new CollisionHandler(EntityType.BULLET, EntityType.DISAPPEARING) {
-            @Override
-            protected void onCollisionBegin(Entity arrow, Entity platform) {
-                arrow.removeFromWorld();
-            }
-        });
-        
-        // 弓箭與刺的碰撞
-        physicsWorld.addCollisionHandler(new CollisionHandler(EntityType.BULLET, EntityType.SPIKE) {
-            @Override
-            protected void onCollisionBegin(Entity arrow, Entity spike) {
-                arrow.removeFromWorld();
-            }
-        });
+            });
+        };
     }
 }
--- goblin-together/src/main/java/com/doggybear/type/EntityType.java
+++ goblin-together/src/main/java/com/doggybear/type/EntityType.java
@@ -5,8 +5,22 @@ package com.doggybear.type;
  */
 public enum EntityType {
     GOBLIN, GOBLIN2, ROPE, // player
-    PLATFORM, BOUNCE, ICE, MOVING, DISAPPEARING, // platform
+    PLATFORM, BOUNCE, ICE, MOVING, DISAPPEARING, FIRE, // platform
     LAVA, // 岩漿
     SPIKE, // 尖刺
-    BULLET, LAUNCHER, // 弓箭發射
+    BULLET, LAUNCHER; // 弓箭發射
+
+    public static final EntityType[] playerTypes = {
+        EntityType.GOBLIN,
+        EntityType.GOBLIN2
+    };
+    
+    public static final EntityType[] removeBulletTypes = {
+        EntityType.PLATFORM,
+        EntityType.MOVING,
+        EntityType.DISAPPEARING,
+        EntityType.ICE,
+        EntityType.FIRE,
+        EntityType.SPIKE
+    };
 }
